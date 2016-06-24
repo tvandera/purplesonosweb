1f7df721449d7a03d0fffb712f8f786f3167673c
Tom Vander Aa <tom.vanderaa@gmail.com>
2016-06-24 19:48:29 +0000

    WakeOnLan: fixes

--- Plugins/WakeOnLan/Plugin.pm
+++ Plugins/WakeOnLan/Plugin.pm
@@ -4,6 +4,7 @@ use Data::Dumper;
 use strict;
 use IO::Handle;
 use Socket;
+use Net::Wake;
 
 
 ###############################################################################
@@ -18,20 +19,8 @@ sub quit {}
 ###############################################################################
 sub wakeup_nas {
         my $mac = '00:17:a4:15:78:cf';
-	# Remove colons
-	$mac =~ tr/://d;
-	# Magic packet is 6 bytes of FF followed by the MAC address 16 times
-	my $magic = ("\xff" x 6) . (pack('H12', $mac) x 16);
-	# Create socket
-	socket(S, PF_INET, SOCK_DGRAM, getprotobyname('udp'))
-		or die "socket: $!\n";
-	# Enable broadcast
-	setsockopt(S, SOL_SOCKET, SO_BROADCAST, 1)
-		or die "setsockopt: $!\n";
-	# Send the wakeup packet
-	defined(send(S, $magic, 0, sockaddr_in(0x2fff, INADDR_BROADCAST)))
-		or print "send: $!\n";
-	close(S);
+	my $ip = '192.168.2.255';
+	Net::Wake::by_udp($ip, $mac);
 }
 
 
@@ -46,7 +35,7 @@ sub av {
     return if (!defined $main::ZONES{$zone}->{AV}->{CurrentTrackMetaData}->{item});
     my $item = $main::ZONES{$zone}->{AV}->{CurrentTrackMetaData}->{item};
     return if ($item->{"upnp:class"} ne "object.item.audioItem.musicTrack");
-    return if ($main::ZONES{$zone}->{AV}->{TransportState} ne "PLAYING");
+    return if ($main::ZONES{$zone}->{AV}->{TransportState} ne "TRANSITIONING");
 
     main::Log (0, "Wakeup NAS");
 
