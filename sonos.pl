0f3318132c4d47b6048cdef7f4d82dcc88078d32
Tom Vander Aa <Tom.VanderAa@gmail.com>
2017-10-05 10:08:01 +0200

    ENH: Search for sonos on specific network: simplication

--- sonos.pl
+++ sonos.pl
@@ -7,7 +7,6 @@ use UPnP::ControlPoint;
 use Socket;
 use IO::Select;
 use IO::Handle;
-use Net::Address::IP::Local;
 use Data::Dumper;
 use HTML::Parser;
 use HTML::Entities;
@@ -214,9 +213,8 @@ sub main {
     $main::useragent = LWP::UserAgent->new(env_proxy  => 1, keep_alive => 2, parse_head => 0);
     $main::daemon = HTTP::Daemon->new(LocalPort => $main::HTTP_PORT, Reuse => 1) || die;
 
-    if ($main::NETWORK) {
-        my $localAddr = Net::Address::IP::Local->connected_to($main::NETWORK);
-        $main::cp = UPnP::ControlPoint->new (SearchAddr => $localAddr);
+    if ($main::SEARCHADDR) {
+        $main::cp = UPnP::ControlPoint->new (SearchAddr => $main::SEARCHADDR);
     } else {
         $main::cp = UPnP::ControlPoint->new ();
     }
