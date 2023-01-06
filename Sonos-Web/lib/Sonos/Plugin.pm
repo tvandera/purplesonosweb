b34c6ec19a6ccf8f9cb98c6100eb1d9600e52bbc
Tom Vander Aa <vanderaa@imec.be>
2023-01-06 19:43:22 +0100

    move more code to Plugin.pm

--- Sonos-Web/lib/Sonos/Plugin.pm
+++ Sonos-Web/lib/Sonos/Plugin.pm
@@ -91,3 +91,103 @@ sub sonos_prefsdb_load {
         }
     }
 }
+
+###############################################################################
+sub add_read_socket {
+    my ($socket, $cb) = @_;
+
+    $main::SOCKETCB{$socket} = $cb;
+    $main::select->add($socket);
+}
+###############################################################################
+sub del_read_socket {
+    my ($socket) = @_;
+
+    delete $main::SOCKETCB{$socket};
+    $main::select->remove($socket);
+}
+###############################################################################
+sub add_timeout {
+    my $time = shift @_;
+    my $cb = shift @_;
+
+    @main::TIMERS = sort { $a->[0] <=> $b->[0] } @main::TIMERS, [$time, $cb, @_];
+}
+###############################################################################
+sub is_timeout_cb {
+    my ($cb) = @_;
+
+    foreach my $item (@main::TIMERS) {
+        return 1 if ($item->[1] == $cb);
+    }
+    return 0;
+}
+
+
+###############################################################################
+sub sonos_add_hook {
+    my $what = shift @_;
+    my $cb   = shift @_;
+
+    push @{$main::HOOK{$what}}, [$cb, @_];
+}
+###############################################################################
+sub sonos_process_hook {
+    my ($what, @other) = @_;
+
+    return undef if (!defined $main::HOOK{$what});
+
+    my @hooks = @{$main::HOOK{$what}};
+
+    while ($#hooks >= 0) {
+        my($callback, @args) = @{shift @hooks};
+        my $out = &$callback($what, @other, @args);
+        return $out if (defined $out);
+    }
+    return undef;
+}
+###############################################################################
+@main::DOWNLOADS = ();
+$main::DOWNLOADS_PID = undef;
+
+sub download
+{
+    my ($url, $file) = @_;
+
+    if (defined $url && defined $file) {
+        Log(2, "invoked ($url, $file)");
+        push @main::DOWNLOADS, [$url, $file];
+    } else {
+        Log(4, "invoked from timer");
+    }
+
+    # No more download, rebuild music
+    if ($#main::DOWNLOADS == -1) {
+        sonos_reindex();
+        return;
+    }
+
+    # Has to be at least one download, keep checking to see when we are done
+    if (!is_timeout_cb(\&download)) {
+        add_timeout (time()+5, \&download);
+    }
+
+    # Already forked so just return
+    return if (defined $main::DOWNLOADS_PID && defined $main::CHLD{$main::DOWNLOADS_PID});
+
+    if ($main::DOWNLOADS_PID = fork) {
+        # Parent
+        Log (4, "Parent");
+        $main::CHLD{$main::DOWNLOADS_PID} = 1;
+        @main::DOWNLOADS = ();
+    } else {
+        # Child
+        Log (4, "Child");
+        my $ua = LWP::UserAgent->new(timeout => 10);
+        foreach my $item (@main::DOWNLOADS) {
+            Log (4, "fetching @{$item}[0] to @{$item}[1]");
+            my $response = $ua->get(@{$item}[0], ":content_file" => @{$item}[1]);
+        }
+        exit;
+    }
+}
