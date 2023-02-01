25415f8511ec86c302be17660776720f5fca9d52
Tom Vander Aa <tom.vanderaa@gmail.com>
2023-02-01 09:31:21 +0100

    daemon.pl: stops when populated

--- Sonos-Web/scripts/daemon.pl
+++ Sonos-Web/scripts/daemon.pl
@@ -2,8 +2,12 @@ use v5.36;
 use strict;
 use warnings;
 
+use Carp;
+$SIG{__DIE__} = \&Carp::confess;
+
 require IO::Async::Handle;
 require IO::Async::Loop::Select;
+use IO::Async::Timer::Periodic;
 
 require Sonos::Discovery;
 
@@ -14,10 +18,22 @@ use Data::Dumper;
 my $loop = IO::Async::Loop::Select->new;
 my $client = Sonos::Discovery->new($loop);
 
-$SIG{INT} = sub {
-    undef $client;
-    undef $loop;
-    exit 0;
-};
+my $timer = IO::Async::Timer::Periodic->new(
+   interval => 1,
+
+   on_tick => sub {
+        return unless $client->populated();
+        print STDERR "All done.\n";
+        my $sec = 5;
+        while ($sec--) {
+            printf "Stopping in %d seconds...\r", $sec;
+            select()->flush();
+            sleep 1
+        }
+        $loop->stop();
+   },
+)->start();
+
+$loop->add($timer);
 
 $loop->run;
