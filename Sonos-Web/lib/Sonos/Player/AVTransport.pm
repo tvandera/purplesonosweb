1532add2c76b395cf243bace9a4b7d012dad3016
Tom Vander Aa <tom.vanderaa@gmail.com>
2023-02-07 08:47:40 +0100

    move AVTransport stuff to AVTransport.pm

--- Sonos-Web/lib/Sonos/Player/AVTransport.pm
+++ Sonos-Web/lib/Sonos/Player/AVTransport.pm
@@ -56,4 +56,85 @@ sub info($self) {
     $self->curTransport()->log($self, " " x 4);
 }
 
+sub isPlaying($self) {
+    return $self->transportState() eq "PLAYING";
+}
+
+sub isStopped($self) {
+    return $self->transportState() eq "STOPPED";
+}
+
+sub startPlaying($self) {
+    return $self->action("Play", "1");
+}
+
+sub stopPlaying($self) {
+    return $self->action("Stop");
+}
+
+sub setURI( $self, $uri, $metadata ) {
+    return $self->action( "SetAVTransportURI", $uri, $metadata );
+}
+
+sub addURI( $self, $uri, $metadata, $queueSlot ) {
+    return $self->action( "AddURIToQueue", $uri, $metadata, $queueSlot );
+}
+
+sub standaloneCoordinator($self) {
+    return $self->action( "BecomeCoordinatorOfStandaloneGroup",);
+}
+
+sub isRepeat($self) {
+    return $self->currentPlayMode() =~ /^REPEAT/;
+}
+
+sub isShuffle($self) {
+    return $self->currentPlayMode() =~ /^SHUFFLE/;
+}
+
+sub switchPlayMode($self, %switch_map) {
+    my %map = (%switch_map, reverse %switch_map);
+    my $new_playmode = $map{$self->currentPlayMode()};
+    $self->action("SetPlayMode", $new_playmode)
+}
+
+# if called with $on_or_off, sets repeat mode to this value
+# if called with $on_of_off == undef, switches repeat mode
+sub setRepeat($self, $on_or_off) {
+    # nothing to do if equal
+    return if $self->getRepeat() == $on_or_off;
+
+    my %switch_repeat = (
+        "NORMAL"  => "REPEAT_ALL",
+        "SHUFFLE" => "SHUFFLE_NOREPEAT",
+    );
+    $self->switchPlayMode(%switch_repeat);
+}
+
+
+# if called with $on_or_off, sets shuffle mode to this value
+# if called with $on_of_off == undef, switches shuffle mode
+sub setShuffle($self, $on_or_off) {
+    # nothing to do
+    return if $self->getShuffle() == $on_or_off;
+
+    my %switch_shuffle = (
+        "NORMAL"     => "SHUFFLE_NOREPEAT",
+        "REPEAT_ALL" => "SHUFFLE",
+    );
+    $self->switchPlayMode(%switch_shuffle);
+}
+
+
+# ---- queue ----
+
+sub seek($self, $queue) {
+    $queue =~ s,^.*/,,;
+    return $self->action("Seek", "TRACK_NR", $queue );
+}
+
+sub removeTrackFromQueue($self, $objectid) {
+    return $self->controlProxy()->RemoveTrackFromQueue( "0", $objectid );
+}
+
 1;
