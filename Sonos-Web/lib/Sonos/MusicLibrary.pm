713d48dceb953e242057af7be38e3fee2b31c893
Tom Vander Aa <tom.vanderaa@gmail.com>
2023-02-22 17:04:49 +0100

    MusicLibrary: remove album art code

--- Sonos-Web/lib/Sonos/MusicLibrary.pm
+++ Sonos-Web/lib/Sonos/MusicLibrary.pm
@@ -20,7 +20,6 @@ use Data::Dumper;
 use Carp;
 
 use constant JSON_BASENAME => "content_cache.json";
-use constant AA_BASENAME => "album_art";
 
 # Contains music library cache
 sub new {
@@ -32,7 +31,6 @@ sub new {
         _updateids => { },
         _items => { },
         _tree => { },
-        _album_art => {},
         _useragent => LWP::UserAgent->new(),
     }, $class;
 
@@ -46,42 +44,19 @@ sub cacheFileName($self) {
     return JSON_BASENAME;
 }
 
-sub albumArtDir($self) {
-    mkdir AA_BASENAME unless -f AA_BASENAME;
-    return AA_BASENAME;
-}
-
 sub load($self) {
     my $filename = $self->cacheFileName();
     return if not -e $filename;
-    my ($updateids, $items, $album_art) = @{decode_json(read_file($filename))};
+    my ($updateids, $items) = @{decode_json(read_file($filename))};
 
     $self->{_updateids} = $updateids if defined $updateids;
     $self->addItemsOnly(@$items) if defined $items;
-
-    for (values %$album_art) {
-        # blob = readfile($sha)
-        $filename = join "/", $self->albumArtDir(), $_->[0];
-        $_->[2] = read_file($filename);
-    }
-
-    $self->{_album_art} = $album_art;
 }
 
 sub save($self) {
     my $filename = $self->cacheFileName();
     #unbless items
     my $allitems = [ map { $_->{_data} } values %{$self->{_items}} ];
-
-    # save albumart w/o binary blobs
-    my %artnoblobs = %{$self->{_album_art}};
-    $_->[2] = undef for values %artnoblobs;
-
-    write_file($filename, encode_json([
-        $self->{_updateids},
-        $allitems,
-        \%artnoblobs,
-    ]));
 }
 
 sub version($self, $id) {
@@ -144,38 +119,6 @@ sub item($self, $id) {
     return $self->{_items}->{$id};
 }
 
-# returns a JPEG/PNG/.. blob
-# - cache key: albumArtURI
-# - cache values: [ sha of blob, mime-type, blob ]
-sub albumArtHelper($self, $item, $player) {
-    $player = $self->playerForID($item->id()) unless $player;
-
-    my $uri = $item->albumArtURI();
-    my $baseurl = $player->location();
-
-    # in cache?
-    my $cache_ref = $self->{_album_art}->{$uri};
-    if (defined $cache_ref) {
-        my ($sha, $mime_type, $blob) = @$cache_ref;
-        return $mime_type, $blob;
-    }
-
-    my $full_uri  = URI::WithBase->new($uri, $baseurl);
-    my $response = $self->{_useragent}->get($full_uri->abs());
-    carp "$uri not found" unless $response->is_success();
-
-    my $mime_type = $response->content_type();
-    my $blob = $response->content;
-    my $sha = sha256_hex($blob);
-    $self->{_album_art}->{$uri} = [ $sha, $mime_type, $blob ];
-    $self->save();
-
-    # also write blob if not yet written
-    my $filename = join "/", $self->albumArtDir, $sha;
-    write_file($filename, $blob) unless -e $filename;
-
-    return $mime_type, $blob;
-}
 
 # only items, no cache id or version info
 sub addItemsOnly($self, @items) {
