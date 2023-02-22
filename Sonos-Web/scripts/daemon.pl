74eb09a5c14b731649c56e69d83cab4f9c55673a
Tom Vander Aa <tom.vanderaa@gmail.com>
2023-02-22 16:43:55 +0100

    WIP: cache locations

--- Sonos-Web/scripts/daemon.pl
+++ Sonos-Web/scripts/daemon.pl
@@ -16,8 +16,14 @@ use Log::Log4perl qw(:easy);
 Log::Log4perl->easy_init($DEBUG);
 use Data::Dumper;
 
+
+my @locations = (
+    'http://192.168.2.200:1400/xml/device_description.xml',
+    'http://192.168.2.102:1400/xml/device_description.xml',
+);
+
 my $loop = IO::Async::Loop::Select->new;
-my $discover = Sonos::Discovery->new($loop);
+my $discover = Sonos::Discovery->new($loop, @locations);
 my $daemon = Sonos::HTTP->new($loop, $discover, LocalAddr => '0.0.0.0', LocalPort => 9999);
 
 $SIG{INT} = sub {
