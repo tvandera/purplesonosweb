327a6b3ca8e07155a716e7e573c506a6fb8e2466
Tom Vander Aa <tom.vanderaa@gmail.com>
2023-02-24 14:14:21 +0100

    AlbumArtCache: store in JSON

--- Sonos-Web/lib/Sonos/AlbumArtCache.pm
+++ Sonos-Web/lib/Sonos/AlbumArtCache.pm
@@ -7,6 +7,7 @@ use warnings;
 require Sonos::MetaData;
 
 use List::Util qw(first reduce);
+use JSON::XS;
 use File::Slurp;
 require URI::WithBase;
 use Digest::SHA qw(sha256_hex);
@@ -23,6 +24,7 @@ use Data::Dumper;
 use Carp;
 
 use constant AA_BASENAME => "album_art";
+use constant JSON_BASENAME => "album_art.json";
 
 # Contains music library cache
 sub new {
@@ -33,15 +35,47 @@ sub new {
         _discovery => $discovery,
         _album_art => {},
         _useragent => LWP::UserAgent->new(),
-        _mime_types =>  MIME::Types->new,
     }, $class;
+
+    $self->load();
 
     return $self;
 }
 
-sub albumArtDir($self) {
+sub cacheDir($self) {
     mkdir AA_BASENAME unless -f AA_BASENAME;
     return AA_BASENAME;
+}
+
+sub cacheFilename($self) {
+    return JSON_BASENAME;
+}
+
+sub load($self) {
+    my $filename = $self->cacheFilename();
+    return if not -e $filename;
+
+    my @items = @{decode_json(read_file($filename))};
+
+    for (@items) {
+        my ($sha, $mime_type, $filename) = @$_;
+        my $full_filename = $self->cacheDir() . "/" . $filename;
+        my $blob = read_file($full_filename);
+        $self->{_album_art}->{$sha} = [ $mime_type, $blob, $filename ];
+    }
+}
+
+sub save($self) {
+    my $filename = $self->cacheFilename();
+
+    # items w/o blob
+    my @items = ();
+    for my $sha (keys %{$self->{_album_art}}) {
+        my ( $mime_type, $blob, $filename) = @{$self->{_album_art}->{$sha}};
+        push @items, [ $sha, $mime_type, $filename ]
+    }
+
+    write_file($filename, encode_json(\@items));
 }
 
 sub system($self) {
@@ -64,43 +98,33 @@ sub extensionOf($self, $blob) {
 # - cache key: albumArtURI
 # - cache values: [ sha of blob, mime-type, blob ]
 sub get($self, $uri) {
+    my $sha = sha256_hex($uri);
+
+    # in memory cache?
+    my $cache_ref = $self->{_album_art}->{$sha};
+    return $sha, @$cache_ref if defined $cache_ref;
 
     # choose a random player to download from
     my @players = $self->system()->players();
     my $player = $players[rand @players];
     my $baseurl = $player->location();
 
-    my $sha = sha256_hex($uri);
-    my $filename;
+    my $full_uri  = URI::WithBase->new($uri, $baseurl);
+    my $response = $self->{_useragent}->get($full_uri->abs());
+    return $sha, undef, undef, undef unless $response->is_success();
 
-    my $mime_type;
-    my $blob;
+    my $blob = $response->content;
+    # Sonos returns the wrong or no mime type, determine from blob
+    my $mime_type = $self->mimeTypeOf($blob);
+    my $filename = sha256_hex($blob) . "." . $self->extensionOf($blob);
+    $self->{_album_art}->{$sha} = [ $mime_type, $blob, $filename ];
 
-    # in memory cache?
-    my $cache_ref = $self->{_album_art}->{$sha};
-    if (defined $cache_ref) {
-        ($mime_type, $blob, $filename) = @$cache_ref;
-    # in file cache?
-    } elsif (my $full_filename = glob $self->albumArtDir() . "/" . $sha . ".*"){
-        $blob = read_file($full_filename);
-        $mime_type = $self->mimeTypeOf($full_filename);
-        $filename = (split "/", $full_filename)[-1];
-        $self->{_album_art}->{$sha} = [ $mime_type, $blob, $filename ];
-    # no? -> download
-    } else {
-        my $full_uri  = URI::WithBase->new($uri, $baseurl);
-        my $response = $self->{_useragent}->get($full_uri->abs());
-        carp "$uri not found" unless $response->is_success();
+    # also write blob to file cache
+    my $full_filename = $self->cacheDir() . "/" . $filename;
+    write_file($full_filename, $blob) unless -e $full_filename;
 
-        $blob = $response->content;
-        # Sonos returns the wrong or no mime type, determine from blob
-        $mime_type = $self->mimeTypeOf($blob);
-        $filename = $sha . "." . $self->extensionOf($blob);
-        $self->{_album_art}->{$sha} = [ $mime_type, $blob, $filename ];
-
-        # also write blob to file cache
-        write_file($self->albumArtDir() . "/" . $filename, $blob);
-    }
+    # write json
+    $self->save();
 
     return $sha, $mime_type, $blob, $filename;
 }
