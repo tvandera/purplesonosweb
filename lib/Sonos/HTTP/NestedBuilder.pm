96a44872a3d10bc940fc64a1ac3055727662dca4
Tom Vander Aa <tom.vanderaa@gmail.com>
2025-04-23 12:20:06 +0200

    unneeded duplicate sort

--- lib/Sonos/HTTP/NestedBuilder.pm
+++ lib/Sonos/HTTP/NestedBuilder.pm
@@ -45,22 +45,7 @@ sub system($self) {
 }
 
 sub players($self, $sorted = 1) {
-    my @players = $self->system()->players();
-
-    @players = sort {
-        # sort by zoneGroup coordinator
-        fc($a->zoneGroupTopology()->coordinator()->friendlyName())
-            cmp
-        fc($b->zoneGroupTopology()->coordinator()->friendlyName())
-        ||
-        # then put coordinator first
-            ($b->zoneGroupTopology()->isCoordinator())
-        ||
-        # then sort by zonename
-            fc($a->friendlyName()) cmp fc($b->friendlyName())
-    }  @players unless !$sorted;
-
-    return @players;
+    return $self->system()->players($sorted);
 }
 
 sub lastUpdate($self) {
@@ -90,58 +75,7 @@ sub to_json {
 
 
 sub build_item_data($self, $item, $player = undef) {
-    my %data;
-    if ($item->populated()) {
-        my $mpath_arg = "";
-        $mpath_arg .= $item->isQueueItem() ?  "queue=" : "mpath=";
-        $mpath_arg .= uri_escape_utf8($item->id()) . "&";
-        $mpath_arg .= "zone=" . $player->friendlyName() . "&" if $player;
-
-        %data = (
-            "id"          => $item->id(),
-            "name"        => encode_entities( $item->title() ),
-            "desc"        => encode_entities( $item->description() ),
-            "artist"      => encode_entities( $item->creator() ),
-            "album"       => encode_entities( $item->album() ),
-            "class"       => encode_entities( $item->class  ),
-            "real_class"  => encode_entities( $item->realClass()  ),
-            "content"     => $item->content,
-            "parent"      => $item->parentID,
-            "albumart"    => $item->albumArtURI,
-            "issong"      => int( $item->isSong() ),
-            "isradio"     => int( $item->isRadio() ),
-            "isalbum"     => int( $item->isAlbum() ),
-            "isfav"       => int( $item->isFav() ),
-            "istop"       => int( $item->isTop() ),
-            "iscontainer" => int( $item->isContainer() ),
-            "isplaylist"  => int( $item->isPlaylist() ),
-            "track_num"   => int( $item->originalTrackNumber() ),
-            "arg"         => $mpath_arg,
-        );
-    } else {
-        %data = (
-            "id"          => "",
-            "id_uri"      => "",
-            "name"        => "",
-            "desc"        => "",
-            "artist"      => "",
-            "album"       => "",
-            "class"       => "",
-            "content"     => "",
-            "parent"      => "",
-            "albumart"    => "",
-            "issong"      => 0,
-            "isradio"     => 0,
-            "isalbum"     => 0,
-            "isfav"       => 0,
-            "iscontainer" => 0,
-            "track_num"   => -1,
-            "arg"         => "",
-        );
-    }
-
-
-    return \%data;
+    return $item->toJSON($player);
 }
 
 sub build_none_data($self) {
@@ -159,75 +93,7 @@ sub build_zones_data($self) {
 sub build_zone_data($self, $player = undef) {
     $player = $self->player() unless $player;
     return {} unless $player;
-
-    my $updatenum = $self->qf("updatenum", -1);
-    my $active_player = $self->player();
-
-    my $render = $player->renderingControl();
-    my $av = $player->avTransport();
-    my $zonename = $player->zoneName();
-
-    my $number_of_tracks = $av->numberOfTracks();
-    my $transportstate = $av->transportState();
-    my %transport_states = ( "TRANSITIONING" => 3, "PAUSED_PLAYBACK" => 2, "PLAYING" => 1, "STOPPED" => 0);
-    my %state_toggles = map { "is_" . $_ => int($transportstate eq $_) } keys %transport_states;
-    my $nexttrack = $av->nextTrack();
-
-    my $zonetopology  = $player->zoneGroupTopology();
-    my $num_linked = $zonetopology->numMembers() - 1;
-    my $fancyname =  $zonename;
-    $fancyname .= " + " . $num_linked if $num_linked;
-
-    return {
-        "has_active_zone"   => int( defined $active_player ),
-
-        "zonename"   => encode_entities( $zonename ),
-        "zoneid"     => uri_escape($player->UDN()),
-        "lastupdate" => $player->lastUpdate(),
-        "updated"    => int( $player->lastUpdate() > $updatenum ),
-
-        "volume"     => $render->getVolume(),
-        "muted"      => $render->getMute(),
-
-        "current_track" => $self->build_item_data($av->metaData(), $player),
-        "title"         => $av->name(),
-        "zone_arg"           => "zone=$zonename&",
-
-        "length"    => $av->lengthInSeconds(),
-        "track_num" => int($av->currentTrack()),
-        "track_tot" => int($number_of_tracks),
-
-        "state"     => $transportstate,
-        "mode"     => $transport_states{$transportstate},
-
-        "repeat"  => $av->isRepeat(),
-        "shuffle" => $av->isShuffle(),
-
-        "next" => $self->build_item_data($nexttrack, $player),
-
-        "icon" => $zonetopology->icon(),
-        "img" =>  "icons" . $zonetopology->icon() . ".png",
-        "lastupdate" => $player->lastUpdate(),
-        "numlinked" => $num_linked,
-        "fancyname" => $fancyname,
-
-        "linked"    => ! $zonetopology->isCoordinator(),
-        "link"      => $zonetopology->coordinator()->UDN(),
-        "link_name" => $zonetopology->coordinator()->friendlyName(),
-
-        "members" => [
-            map {
-                my $uuid = $_->{"UUID"};
-                {
-                    "name"   => $zonetopology->zoneName($uuid),
-                    "id"     => $uuid,
-                    "linked" => int( ! $zonetopology->isCoordinator($uuid) ),
-                    "icon"   => $zonetopology->icon($uuid),
-                    "img"    => "icons" . $zonetopology->icon($uuid) . ".png",
-                }
-            } $zonetopology->members()
-        ]
-    };
+    return $player->toJSON();
 }
 
 sub build_info_data {
@@ -238,43 +104,14 @@ sub build_info_data {
 sub build_queue_data($self) {
     my $player = $self->player();
     return {} unless $player;
-
-    my $queue = $player->queue();
-    my $updatenum = $self->qf("updatenum", -1);
-
-    return {
-        "zone"       => $player->zoneName,
-        "zoneid"     => uri_escape_utf8($player->UDN),
-        "lastupdate" => $queue->lastUpdate(),
-        "updated"    => int( $queue->lastUpdate() > $updatenum ),
-        "queue"      => [ map { $self->build_item_data($_, $player) } $queue->items() ],
-    };
+    return $player->queue()->toJSON();
 }
 
 sub build_music_data($self) {
-    my $updatenum = $self->qf("updatenum", -1);
-
-    my $mpath = $self->qf("mpath", "");
-    $mpath    = "" if ( $mpath eq "/" );
-
-    my $music      = $self->system()->musicLibrary();
-    my $parent     = $music->item($mpath);
-
-    my @elements;
-    if (my $msearch = $self->qf("msearch")) {
-        @elements   = $music->search($msearch);
-    } else {
-        @elements   = $music->children($parent);
-    }
-
-    return {
-        "root"       => int( $mpath eq "" ),
-        "lastupdate" => $music->lastUpdate(),
-        "updated"    => int( $music->lastUpdate() > $updatenum ),
-        "path"       => encode_entities($mpath),
-        "item"       => $self->build_item_data($parent),
-        "children"   => [ map { $self->build_item_data($_) } @elements ]
-    }
+    my $music   = $self->system()->musicLibrary();
+    my $mpath   = $self->qf("mpath", "");
+    my $msearch = $self->qf("msearch");
+    return $music->toJSON($mpath, $msearch);
 }
 
 sub build_globals_data($self) {
