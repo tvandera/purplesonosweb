73761159bb1da367373e6036dc53af48ec5ef85a
Tom Vander Aa <tom.vanderaa@gmail.com>
2025-04-24 10:42:55 +0200

    cli (wip)

--- scripts/cli.pl
+++ scripts/cli.pl
@@ -3,92 +3,111 @@ use strict;
 use warnings;
 use LWP::UserAgent;
 use JSON;
+use JSON::Path;
 use URI::Escape;
 use HTML::Entities;
 use Text::Table;
 
 binmode STDOUT, ":encoding(UTF-8)";
 
-my ($zone, $command, @args) = @ARGV;
-
-my $base_url = "http://127.0.0.1:9999/api";
-my $ua = LWP::UserAgent->new;
-my %params;
-
-if (!$zone && !$command) {
-    $command = "zones";
-} elsif ($zone && $command) {
-    $params{zone} = $zone;
-} else {
-    usage();
-}
+dispatch();
 
 # --- Command Mapping ---
-my @actions = qw(play pause stop next previous volume mute unmute search);
-my @info = qw(queue info music all zones);
+sub dispatch {
+    my @actions = qw(play pause stop next previous volume mute unmute);
+    my @global = qw(music search all zones);
+    my @per_zone = qw(queue zone);
 
-if (grep { $_ eq $command } @actions) {
-    $params{action} = $command;
-    $params{nowait} = "1";
-} elsif (grep { $_ eq $command } @info) {
-    $params{what} = $command;
-} else {
-    usage("Unknown command: $command");
+    global_info("zones") if !scalar @ARGV;
+    global_info(@ARGV) if (grep { $_ eq $ARGV[0] } @global);
+
+    my ($zone, $command, @args) = @ARGV;
+    usage() unless $zone && $command;
+
+    zone_info($zone, $command, @args) if (grep { $_ eq $command } @per_zone);
+    zone_command($zone, $command, @args) if (grep { $_ eq $command } @actions);
 }
 
-if ($command eq 'volume') {
-    usage("Missing volume level") unless defined $args[0];
-    $params{volume} = $args[0];
+sub global_info {
+    my ( $what, @args ) = @_;
+    my %params = ("what" => $what);
+
+    if ($what eq 'search') {
+        usage("Missing search term") unless defined $args[0];
+        $params{msearch} = $args[0];
+    }
+
+    if ($what eq 'music') {
+        $params{mpath} = $args[0] ? $args[0] : "";
+    }
+
+    my $json = do_request(%params);
+
+    show_info($what, $json);
 }
 
-if ($command eq 'search') {
-    usage("Missing search term") unless defined $args[0];
-    $params{msearch} = $args[0];
+sub zone_info {
+    my ( $zone, $what, @args ) = @_;
+    my %params = ("zone" => $zone, "what" => $what);
+    my $json = do_request(%params);
 }
 
-if ($command eq 'music') {
-    $params{mpath} = $args[0] ? $args[0] : "";
+sub zone_command {
+    my ( $zone, $command, @args ) = @_;
+    my %params = ("zone" => $zone, "action" => $command);
+
+    if ($command eq 'volume') {
+        usage("Missing volume level") unless defined $args[0];
+        $params{volume} = $args[0];
+    }
+
+    my $json = do_request(%params);
 }
 
-# --- Build query URL ---
-my $query = join '&', map { uri_escape($_) . '=' . uri_escape($params{$_}) } keys %params;
-my $url = "$base_url?$query";
+sub do_request {
+    my $base_url = "http://127.0.0.1:9999/api";
+    my $ua = LWP::UserAgent->new;
 
-# --- Make the request ---
-my $res = $ua->get($url);
-die "Request failed: " . $res->status_line unless $res->is_success;
+    my %params = @_;
 
-my $json = decode_json($res->decoded_content);
+    # --- Build query URL ---
+    my $query = join '&', map { uri_escape($_) . '=' . uri_escape($params{$_}) } keys %params;
+    my $url = "$base_url?$query";
+
+    # --- Make the request ---
+    my $res = $ua->get($url);
+    die "Request failed: " . $res->status_line unless $res->is_success;
+
+    my $json = decode_json($res->decoded_content);
+    return $json;
+}
 
 # --- Output formatting ---
-if ($command eq 'queue' && $json->{queue}) {
-    my @fields = qw(class name artist album streamContent);
-    print_table("Queue", $json->{queue}, \@fields);
-}
-elsif ($command eq 'music' && $json->{children}) {
-    my @fields = qw(id name);
-    print_table("Music", $json->{children}, \@fields);
-}
-elsif ($command eq 'zones' && $json->{loop}) {
-    my @fields = qw(zone_name active_state active_name);
-    print_table("Zones", $json->{zones_loop}, \@fields);
-}
-elsif ($command eq 'info') {
-    printf "Zone: %s | Volume: %d | Muted: %s | Status: %s | Track: %s\n",
-        $json->{name},
-        $json->{volume},
-        $json->{muted} ? "yes" : "no",
-        ($json->{PLAYING} ? "playing" : $json->{active_STOPPED} ? "stopped" : "paused"),
-        $json->{name} || "-";
-}
-elsif ($command =~ /^(play|pause|stop|next|previous|volume|mute|unmute)$/) {
-    print "Action '$command' sent to zone '$zone'.\n";
-}
-else {
-    print to_json($json, { pretty => 1 });
-}
+sub show_info {
+    my ( $command, $json ) = @_;
 
-# --- Helpers ---
+    my %field_map = (
+        "queue" => [ qw(class name artist album streamContent) ],
+        "music" => [ '$..name' ],
+        "zones" => [ qw(zone/name active_state active_name)],
+    );
+
+    print $command, ":\n", to_json($json, { pretty => 1 });
+
+    my @fields = $field_map{$command};
+    if (@fields) {
+        print_table($command, $json, \@fields)
+    } elsif ($command eq 'zone') {
+        printf "Zone: %s | Volume: %d | Muted: %s | State: %s\n",
+            $json->{zone}->{name},
+            $json->{render}->{volume},
+            $json->{render}->{muted} ? "yes" : "no",
+            $json->{av}->{transport_state}
+    }
+    else {
+        print to_json($json, { pretty => 1 });
+    }
+}
 
 sub print_table {
     my ($title, $data, $cols) = @_;
