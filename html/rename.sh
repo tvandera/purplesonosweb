312f6164d2ed9ef04315a2b2306f80dcfae19189
Tom Vander Aa <tom.vanderaa@gmail.com>
2025-05-07 09:29:35 +0200

    v1 -> v2

--- html/rename.sh
+++ html/rename.sh
@@ -5,6 +5,9 @@ REPLACEMENTS="
     music_arg/args.mpath
     all_arg/args.all
     has_active_zone/args.zone
+    music_(\\w+)/music.\\1
+    queue_(\\w+)/item.\\1
+    ZONE_FANCYNAME/player.zone.name
     "
 
 for R in $REPLACEMENTS
