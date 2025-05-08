a19a0474d6ef65c836c3652ba9cfe02465fadba1
Tom Vander Aa <tom.vanderaa@gmail.com>
2025-05-08 08:34:33 +0200

    template-scanner: template file names args

--- scripts/template-scanner.pl
+++ scripts/template-scanner.pl
@@ -8,29 +8,19 @@ use Template;
 use File::Slurp;
 use File::Basename;
 
-
-my @files = glob('
-    html/*.html
-    html/*/*.html
-    html/*.json
-    html/*/*.json
-    html/*.js
-    html/*/*.js
-    html/*.xml
-    html/*/*.xml');
-
 my $decoder = JSON->new()->utf8();
 my $fname = shift @ARGV;
 my $input = read_file($fname);
 my $json = $decoder->decode($input);
 
-for my $fname (@files) {
+for my $fname (@ARGV) {
    print("\n=======  $fname =======\n");
 
    my $output = '';
     my $tt = Template->new({
-        STRICT => 1,
+        STRICT => 0,
         INCLUDE_PATH => [ '.', dirname($fname) ],
+        DEBUG => 'undef',
         RELATIVE => 1,
     });
    my $ok = $tt->process($fname, $json, \$output);
